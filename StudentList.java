7b35fbfdc3f6953f317abc3d6356a4f64390424e
LAPTOP-7R2R2SKL\Asus <rafat.hossain.reyal@gmail.com>
2022-11-14 19:56:48 +0600

    #4 task is done

--- StudentList.java
+++ StudentList.java
@@ -8,37 +8,30 @@ public class StudentList {
 //		Check arguments
         if (args[0].equals(Constant.showAll)) {
             System.out.println(Constant.loadingDataMsg);
-            try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.txtfileName)));
-                String line = bufferedReader.readLine();
+
+                BufferedReader bufferedReader =readFile(Constant.txtfileName);
+                String line = readOneFile(bufferedReader);
                 String studentNames[] = line.split(Constant.coma);
                 for (String studentName : studentNames) {
                     System.out.println(studentName.trim());
                 }
-            } catch (Exception e) {
-            }
+
             System.out.println(Constant.dataLoadedMsg);
         } else if (args[0].equals(Constant.random)) {
             System.out.println(Constant.loadingDataMsg);
-            try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.txtfileName)));
-                String line = bufferedReader.readLine();
+
+                BufferedReader bufferedReader =readFile(Constant.txtfileName);
+                String line = readOneFile(bufferedReader);
                 String studentNames[] = line.split(Constant.coma);
                 Random random = new Random();
                 int studentNumber = random.nextInt(4);
                 System.out.println(studentNames[studentNumber].trim());
-            } catch (Exception e) {
-            }
+
             System.out.println(Constant.dataLoadedMsg);
         } else if (args[0].contains(Constant.add)) {
             System.out.println(Constant.loadingDataMsg);
             try {
-                BufferedWriter bufferedWriter = new BufferedWriter(
-                        new FileWriter(Constant.txtfileName, true));
+                BufferedWriter bufferedWriter = writeFile(Constant.txtfileName);
                 String newStudentName = args[0].substring(1);
                 Date date = new Date();
                 String dateFormate = Constant.dateFormat;
@@ -52,34 +45,62 @@ public class StudentList {
             System.out.println(Constant.dataLoadedMsg);
         } else if (args[0].contains(Constant.search)) {
             System.out.println(Constant.loadingDataMsg);
-            try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.txtfileName)));
-                String line = bufferedReader.readLine();
+                BufferedReader bufferedReader = readFile(Constant.txtfileName);
+                String line = readOneFile(bufferedReader);
                 if (line.contains(args[0].substring(1))) {
                     System.out.println(Constant.foundMsg);
                 } else {
                     System.out.println(Constant.notFoundMsg);
                 }
-            } catch (Exception e) {
-            }
+
             System.out.println(Constant.dataLoadedMsg);
         } else if (args[0].contains(Constant.count)) {
             System.out.println(Constant.loadingDataMsg);
-            try {
-                BufferedReader bufferedReader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.txtfileName)));
-                String line = bufferedReader.readLine();
+
+                BufferedReader bufferedReader = readFile(Constant.txtfileName);
+                String line = readOneFile(bufferedReader);
                 String names[] = line.split(Constant.coma);
                 System.out.println(names.length / 2 + Constant.wordFound);
-            } catch (Exception e) {
-            }
+
             System.out.println(Constant.dataLoadedMsg);
         }
         else{
             System.out.println(Constant.warningMsg);
+        }
+    }
+    static BufferedReader readFile(String fileName)
+    {
+        try
+        {
+            return new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(fileName)));
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+    static String readOneFile(BufferedReader bufferedReader)
+    {
+        try
+        {
+            return bufferedReader.readLine();
+        }catch (Exception e)
+        {
+            return null;
+        }
+    }
+    static BufferedWriter writeFile(String fileName)
+    {
+        try
+        {
+            return new BufferedWriter(
+                    new FileWriter(fileName, true));
+        }
+        catch (Exception e)
+        {
+            return null;
         }
     }
 }
