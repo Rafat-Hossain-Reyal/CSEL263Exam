8e6c4a2a031b341e940bb28321ea40a1a6e6510d
LAPTOP-7R2R2SKL\Asus <rafat.hossain.reyal@gmail.com>
2022-11-14 18:17:28 +0600

    #2 task is done

--- StudentList.java
+++ StudentList.java
@@ -78,5 +78,8 @@ public class StudentList {
             }
             System.out.println("Data Loaded.");
         }
+        else{
+            System.out.println("Usage:a|c|r|+Studenti|?StudentI");
+        }
     }
 }
